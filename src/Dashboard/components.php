4f029b11574ce21158aa79548ed7cf390b35206c
Hugo Vieira <hugo.vieira2002@hotmail.com>
2026-06-17 08:49:40 +0100

    chore: ui change

--- src/Dashboard/components.php
+++ src/Dashboard/components.php
@@ -46,7 +46,7 @@ HTML;
 
 function showcase_preview(string $id): string
 {
-    return '<div id="' . h($id) . '" class="showcase-preview border rounded bg-body-tertiary d-flex align-items-center justify-content-center p-4"></div>';
+    return '<div id="' . h($id) . '" class="showcase-preview border rounded bg-body-tertiary d-flex align-items-center justify-content-center p-4 h-100"></div>';
 }
 
 function empty_panel(string $text): string
