45ff4c8ad39eacfd1d398a4ffbc37dcb10d7e84c
Hugo Vieira <hugo.vieira2002@hotmail.com>
2026-06-18 15:30:30 +0100

    feat: project logo

--- src/Dashboard/index.php
+++ src/Dashboard/index.php
@@ -16,6 +16,9 @@ require_once __DIR__ . '/components/modal.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hitecosystem Hub de Dispositivos</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/logo.svg">
+    <link rel="icon" type="image/svg+xml" sizes="32x32" href="/assets/logo.svg">
+    <link rel="icon" type="image/svg+xml" sizes="16x16" href="/assets/logo.svg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
     <style>
